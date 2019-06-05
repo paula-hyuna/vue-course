4ab132f40fb98a4974d5c5d23a8713cb1b61f0be
Paula Melero <paula.melero.ramirez@hyuna.bb>
2019-06-05 11:56:53 -0400

    first commit

---
+++ exercises/Exercise-6/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+           <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue';
+    import Servers from './components/Server/Servers.vue';
+    import Details from './components/Server/Details.vue';
+    import Footer from './components/Shared/Footer.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': Details,
+            'app-footer': Footer
+        }   
+    }
+</script>
+
+<style>
+
+</style>
---
+++ exercises/Exercise-6/src/components/Server/Servers.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="value in servers" v-bind:key="value.name">
+                {{ value.name }} | Status: {{value.status}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {
+                        name: 'Server #1',
+                        status: 'Normal'
+                    },
+                    {
+                        name: 'Server #2',
+                        status: 'Critical'
+                    },
+                    {
+                        name: 'Server #3',
+                        status: 'Critical'
+                    },
+                    {
+                        name: 'Server #4',
+                        status: 'Normal'
+                    },
+                    {
+                        name: 'Server #5',
+                        status: 'Normal'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+/* not only one global DOM but many separate DOMs 
+we see data-v-1d3b3738 as style property on our page when we click inspect
+this is created by vue. All styles are included in the head section of the document
+automatically. 
+*/
+    div{
+        border: 1px solid blue;
+    }
+</style>
